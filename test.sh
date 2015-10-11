f0a4bd538cedb59b196da0061c97e9a468650a9c
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2015-10-11 14:25:36 +0200

    Daisychain the pynnless unit tests

--- test.sh
+++ test.sh
@@ -2,3 +2,8 @@
 
 # Automatically run and discover unittests
 python -m unittest discover
+
+# If pynnless is present in the lib/ folder, run its tests too
+if [ -e lib/pynnless/test.sh ]; then
+	(cd lib/pynnless; ./test.sh)
+fi
