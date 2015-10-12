67388ad635ed175fe950f289cf2a576a64c4f12f
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2015-10-12 13:23:38 +0200

    Abort test chaining on error

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Automatically run and discover unittests
-python -m unittest discover
+python -m unittest discover || exit 1
 
 # If pynnless is present in the lib/ folder, run its tests too
 if [ -e lib/pynnless/test.sh ]; then
-	(cd lib/pynnless; ./test.sh)
+	(cd lib/pynnless; ./test.sh) || exit 1
 fi
