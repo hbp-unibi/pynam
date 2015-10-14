19e9ecb22a1148c9bf0275119f0a04fb37acb1ab
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2015-10-14 02:21:38 +0200

    improved clean.sh

--- clean.sh
+++ clean.sh
@@ -1,3 +1,31 @@
 #!/bin/sh
+#   PyNNLess -- Yet Another PyNN Abstraction Layer
+#   Copyright (C) 2015 Andreas Stöckel
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-find -depth \( \( -name "*.aux" -o -name "*.snm" -o -name "*.out" -o -name "*.toc" -o -name "*.nav" -o -name "*.log" -o -name "*.backup" -o -name "*.bbl" -o -name "*.blg" -o -name "*~" -o -name "CMakeCache.txt" -o -name "*.pyc" \) -a \! \( -wholename "*.git/*" -o -wholename "*.svn" \) \) -delete -print
+find -depth \(\
+		\(\
+			   -name "*.backup"\
+			-o -name "*~"\
+			-o -name "*.pyc"\
+			-o -name "spiketrain.in"\
+			-o -name "spikeyconfig.out"\
+			-o -name "logfile.txt"\
+			-o -wholename "*reports*"\
+			-o -wholename "*application_generated_data*"\
+		\) -a \! \(\
+			   -wholename "*.git/*"\
+			-o -wholename "*.svn" \)\
+		\) -delete -print
