9cb86391ed80ddce7a1b3393668c5a98866ee230
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2015-10-18 17:43:02 +0200

    Add utility code for restoring python dictionaries from HDF5 files

--- pynam/utils.py
+++ pynam/utils.py
@@ -24,6 +24,7 @@ Contains various utility methods used in various places of the program.
 import re
 import json
 import numpy as np
+import scipy.io as scio
 import pynnless.pynnless_utils
 
 def initialize_seed(seed, seq=1):
@@ -65,6 +66,42 @@ def parse_json_with_comments(stream):
         content = content[:match.start()] + content[match.end():]
         match = JSON_COMMENT_RE.search(content)
     return json.loads(content)
+
+# loadmat with python dictionaries
+# See http://stackoverflow.com/questions/7008608/scipy-io-loadmat-nested-structures-i-e-dictionaries
+
+def loadmat(filename):
+    '''
+    this function should be called instead of direct scio.loadmat
+    as it cures the problem of not properly recovering python dictionaries
+    from mat files. It calls the function check keys to cure all entries
+    which are still mat-objects
+    '''
+    data = scio.loadmat(filename, struct_as_record=False, squeeze_me=True)
+    return _check_keys(data)
+
+def _check_keys(dict):
+    '''
+    checks if entries in dictionary are mat-objects. If yes
+    todict is called to change them to nested dictionaries
+    '''
+    for key in dict:
+        if isinstance(dict[key], scio.matlab.mio5_params.mat_struct):
+            dict[key] = _todict(dict[key])
+    return dict
+
+def _todict(matobj):
+    '''
+    A recursive function which constructs from matobjects nested dictionaries
+    '''
+    dict = {}
+    for strg in matobj._fieldnames:
+        elem = matobj.__dict__[strg]
+        if isinstance(elem, scio.matlab.mio5_params.mat_struct):
+            dict[strg] = _todict(elem)
+        else:
+            dict[strg] = elem
+    return dict
 
 # Forward the "init_key" method
 init_key = pynnless.pynnless_utils.init_key
