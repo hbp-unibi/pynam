ec02521efaa7288f74773e1e06c0f8093e3994cc
Andreas Stöckel <astoecke@techfak.uni-bielefeld.de>
2015-11-03 16:57:08 +0100

    Better 2D plots

--- misc/information_plot.py
+++ misc/information_plot.py
@@ -22,6 +22,7 @@ Plots the information for an experiment result as created with ./run.py
 """
 
 import numpy as np
+import math
 import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
@@ -155,7 +156,8 @@ def plot_measure(ax, xs, ys, ys_std, color, simulator, xlabel, ylabel,
         ax.set_ylabel(ylabel)
         ax.set_ylim(bottom=0)
 
-def plot_measure2d(ax, xs, ys, zs, simulator, xlabel, ylabel):
+def plot_measure2d(ax, xs, ys, zs, simulator, xlabel, ylabel, vmin=None,
+        vmax=None, fmt='%.0f\\%%'):
 
     midx = lambda m: keys.index(m)
 
@@ -167,11 +169,25 @@ def plot_measure2d(ax, xs, ys, zs, simulator, xlabel, ylabel):
     ys = ys.reshape((steps_x, steps_y))
     zs = zs.reshape((steps_x, steps_y))
 
-    levels = np.linspace(0.0, 100.0, 11) # 10% steps
+    # Auto-scale
+    idcs = zs != np.inf
+    if np.sum(idcs) == 0:
+        return
+    if vmin is None:
+        vmin = np.min(zs[idcs])
+    if vmax is None:
+        vmax = np.max(zs[idcs])
 
-    CS1 = ax.contourf(xs, ys, zs, levels, cmap="Blues")
+    # Select the colormap
+    cmap = "Purples"
+    if vmin < 0.0:
+        cmap = "PuOr"
+
+    steps = math.pow(10.0, round(math.log10(vmax - vmin)) - 1.0)
+    levels = np.arange(vmin, vmax, steps)
+    CS1 = ax.contourf(xs, ys, zs, levels, cmap=cmap, vmin=vmin, vmax=vmax)
     CS2 = ax.contour(xs, ys, zs, levels, colors='k')
-    CS2.levels = map(lambda val: '%.0f' % val, levels)
+    CS2.levels = map(lambda val: fmt % val, levels)
     ax.clabel(CS2, inline=1, fontsize=8)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
@@ -190,53 +206,62 @@ def plot_1d(xlabel, means, stds, simulator, keys):
         simulator = SIMULATOR_LABELS[simulator]
 
     # Plot the information metric
-    ax, first, _ = get_figure(experiment, "info", simulator)
-    plot_measure(ax, xs=means[:, 0], ys=means[:, midx("I")],
-            ys_std=stds[:, midx("I")], color=color, simulator=simulator,
-            xlabel=xlabel, ylabel="Storage capacity $S$ [bit]",
-            ys_ref=means[:, midx("I_ref")], first=first)
+    if "I" in keys:
+        ax, first, _ = get_figure(experiment, "info", simulator)
+        plot_measure(ax, xs=means[:, 0], ys=means[:, midx("I")],
+                ys_std=stds[:, midx("I")], color=color, simulator=simulator,
+                xlabel=xlabel, ylabel="Storage capacity $S$ [bit]",
+                ys_ref=means[:, midx("I_ref")], first=first)
 
     # Plot the information metric (normalized)
-    ax, first, _ = get_figure(experiment, "info_n", simulator)
-    plot_measure(ax, xs=means[:, 0], ys=means[:, midx("I_n")] * 100.0,
-            ys_std=stds[:, midx("I_n")] * 100.0, color=color, simulator=simulator,
-            xlabel=xlabel, ylabel="Storage capacity $S$ [\\%]", first=first)
+    if "I_n" in keys:
+        ax, first, _ = get_figure(experiment, "info_n", simulator)
+        plot_measure(ax, xs=means[:, 0], ys=means[:, midx("I_n")] * 100.0,
+                ys_std=stds[:, midx("I_n")] * 100.0, color=color,
+                simulator=simulator,
+                xlabel=xlabel, ylabel="Storage capacity $S$ [\\%]", first=first)
 
     # Plot the number of false positives
-    ax, first, _ = get_figure(experiment, "fp", simulator)
-    plot_measure(ax, xs=means[:, 0], ys=means[:, midx("fp")],
-            ys_std=stds[:, midx("fp")], color=color, simulator=simulator,
-            xlabel=xlabel, ylabel="False positives $f_p$ [bit]",
-            ys_ref=means[:, midx("fp_ref")], first=first)
+    if "fp" in keys:
+        ax, first, _ = get_figure(experiment, "fp", simulator)
+        plot_measure(ax, xs=means[:, 0], ys=means[:, midx("fp")],
+                ys_std=stds[:, midx("fp")], color=color, simulator=simulator,
+                xlabel=xlabel, ylabel="False positives $f_p$ [bit]",
+                ys_ref=means[:, midx("fp_ref")], first=first)
 
     # Plot the number of false positives (normalized)
-    ax, first, _ = get_figure(experiment, "fp_n", simulator)
-    plot_measure(ax, xs=means[:, 0], ys=means[:, midx("fp_n")] * 100.0,
-            ys_std=stds[:, midx("fp_n")] * 100.0, color=color,
-            simulator=simulator,
-            xlabel=xlabel, ylabel="False positives $f_p$ [\\%]",
-            ys_ref=means[:, midx("fp_ref_n")] * 100.0, first=first)
+    if "fp_n" in keys:
+        ax, first, _ = get_figure(experiment, "fp_n", simulator)
+        plot_measure(ax, xs=means[:, 0], ys=means[:, midx("fp_n")] * 100.0,
+                ys_std=stds[:, midx("fp_n")] * 100.0, color=color,
+                simulator=simulator,
+                xlabel=xlabel, ylabel="False positives $f_p$ [\\%]",
+                ys_ref=means[:, midx("fp_ref_n")] * 100.0, first=first)
 
     # Plot the number of false negatives
-    ax, first, _ = get_figure(experiment, "fn", simulator)
-    plot_measure(ax, xs=means[:, 0], ys=means[:, midx("fn")],
-            ys_std=stds[:, midx("fn")], color=color, simulator=simulator,
-            xlabel=xlabel, ylabel="False negatives $f_n$ [bit]",
-            first=first)
+    if "fn" in keys:
+        ax, first, _ = get_figure(experiment, "fn", simulator)
+        plot_measure(ax, xs=means[:, 0], ys=means[:, midx("fn")],
+                ys_std=stds[:, midx("fn")], color=color, simulator=simulator,
+                xlabel=xlabel, ylabel="False negatives $f_n$ [bit]",
+                first=first)
 
     # Plot the number of false negatives (normalized)
-    ax, first, _ = get_figure(experiment, "fn_n", simulator)
-    plot_measure(ax, xs=means[:, 0], ys=means[:, midx("fn_n")] * 100.0,
-            ys_std=stds[:, midx("fn_n")] * 100.0, color=color,
-            simulator=simulator, xlabel=xlabel,
-            ylabel="False negatives $f_n$ [\\%]", first=first)
+    if "fn_n" in keys:
+        ax, first, _ = get_figure(experiment, "fn_n", simulator)
+        plot_measure(ax, xs=means[:, 0], ys=means[:, midx("fn_n")] * 100.0,
+                ys_std=stds[:, midx("fn_n")] * 100.0, color=color,
+                simulator=simulator, xlabel=xlabel,
+                ylabel="False negatives $f_n$ [\\%]", first=first)
 
     # Plot the latencies
-    ax, first, id_ = get_figure(experiment, "latency", simulator)
-    plot_measure(ax, xs=means[:, 0], ys=means[:, dims + 5],
-            ys_std=stds[:, dims + 5], color=color, simulator=simulator,
-            xlabel=xlabel, ylabel="Latency $\\delta$ [ms]",
-            first=first)
+    if "lat_avg" in keys:
+        ax, first, id_ = get_figure(experiment, "latency", simulator)
+        plot_measure(ax, xs=means[:, 0], ys=means[:, midx("lat_avg")],
+                ys_std=stds[:, midx("lat_avg")], color=color,
+                simulator=simulator, xlabel=xlabel,
+                ylabel="Latency $\\delta$ [ms]", first=first)
+
 
 def plot_2d(xlabel, ylabel, means, stds, simulator, keys):
     dims = 2
@@ -245,11 +270,43 @@ def plot_2d(xlabel, ylabel, means, stds, simulator, keys):
     figsize = (cm2inch(10), cm2inch(10))
 
     # Plot the information metric
-    ax, _, _ = get_figure(experiment, "info" + "_2d_" + simulator, simulator,
-            figsize=figsize)
-    plot_measure2d(ax, xs=means[:, 0], ys=means[:, 1],
-            zs=means[:, midx("I_n")] * 100.0, simulator=simulator,
-            xlabel=xlabel, ylabel=ylabel)
+    if "I_n" in keys:
+        ax, _, _ = get_figure(experiment, "info_n" + "_2d_" + simulator,
+                simulator, figsize=figsize)
+        plot_measure2d(ax, xs=means[:, 0], ys=means[:, 1],
+                zs=means[:, midx("I_n")] * 100.0, simulator=simulator,
+                xlabel=xlabel, ylabel=ylabel, vmin=0.0, vmax=100.0)
+
+    # Plot the false positives metric
+    if ("fp_n" in keys) and ("fp_ref_n" in keys):
+        ax, _, _ = get_figure(experiment, "fp_n" + "_2d_" + simulator,
+                simulator, figsize=figsize)
+        plot_measure2d(ax, xs=means[:, 0], ys=means[:, 1],
+                zs=(means[:, midx("fp_n")] - means[:, midx("fp_ref_n")]) * 100,
+                simulator=simulator, xlabel=xlabel, ylabel=ylabel,
+                vmin=-100.0, vmax=100.0)
+
+    # Plot the false negatives metric
+    if "fn_n" in keys:
+        ax, _, _ = get_figure(experiment, "fn_n" + "_2d_" + simulator,
+                simulator, figsize=figsize)
+        plot_measure2d(ax, xs=means[:, 0], ys=means[:, 1],
+                zs=means[:, midx("fn_n")] * 100.0,
+                simulator=simulator, xlabel=xlabel, ylabel=ylabel,
+                vmin=0.0, vmax=100.0)
+
+    # Plot the latency metric
+    if "lat_avg" in keys:
+        ax, _, _ = get_figure(experiment, "lat_avg" + "_2d_" + simulator,
+                simulator, figsize=figsize)
+        plot_measure2d(ax, xs=means[:, 0], ys=means[:, 1],
+                zs=means[:, midx("lat_avg")],
+                simulator=simulator, xlabel=xlabel, ylabel=ylabel, vmin=0.0,
+                fmt="%.2f")
+
+#
+# Main entry point
+#
 
 for target_file in sys.argv[1:]:
     print "Processing " + target_file
